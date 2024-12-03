c7fae6491d133679ba471d3161527ca3fa69cae4
Hemanthuppal <131530809+Hemanthuppal@users.noreply.github.com>
2024-12-03 16:23:33 +0530

    Update Api.js

--- React/src/Components/Apiurl/Api.js
+++ React/src/Components/Apiurl/Api.js
@@ -1,12 +1,3 @@
-import axios from 'axios';
 
-// Define the BASE_URL
 export const BASE_URL = 'https://23.23.27.157';
 
-// Create and configure Axios instance
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  httpsAgent: new (require('https').Agent)({
-    rejectUnauthorized: false, // Disable SSL verification (development only)
-  }),
-});
